975c0962631ca0ec251373d2385c929c6f5f8f44
anirut-th <thammaviset.anirut@gmail.com>
2020-11-19 09:41:32 +0700

    Add models

---
+++ AwesomeBank/AwesomeBankAPI/Models/Account.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AwesomeBankAPI.Models
+{
+    public class Account
+    {
+        public Guid Id { get; set; }
+        public Guid CustomerId { get; set; }
+        public string Iban { get; set; }
+        public string Name { get; set; }
+        public decimal BalanceAmount { get; set; }
+        public bool IsActive { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
---
+++ AwesomeBank/AwesomeBankAPI/Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AwesomeBankAPI.Models
+{
+    public class Customer
+    {
+        public Guid Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public bool IsActive { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
